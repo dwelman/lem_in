0c5b4247abb1657353f62e8720de537cd457412e
David WELMAN <daviwel@e5r7p1.jnb.42.fr>
2016-07-19 10:24:38 +0200

    a

--- src/control_ants.c
+++ src/control_ants.c
@@ -6,7 +6,7 @@
 /*   By: daviwel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/19 07:26:21 by daviwel           #+#    #+#             */
-/*   Updated: 2016/07/19 10:06:34 by daviwel          ###   ########.fr       */
+/*   Updated: 2016/07/19 10:24:09 by daviwel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,12 +37,13 @@ void	control_ants(t_info *info)
 			temp_node = (t_node *)temp_path->next->data;
 			if (temp_node->has_ant == 0)
 			{
-				ft_printf("HAI\n");
-				ft_lstappend(&info->ants, (void *)make_ant(cur_ant, temp_path));
+				ft_lstappend(&info->ants, ft_lstnew((void *)make_ant(cur_ant,
+								temp_path)));
 				cur_ant++;
 			}
 			path_crawl = path_crawl->next;
 		}
 		move_all_ants(info->ants, info->end);
+		ft_printf("HAI\n");
 	}
 }
